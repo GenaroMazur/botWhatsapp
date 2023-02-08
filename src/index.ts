e1f0499908cc4664d72adf772c97803ba09abb8a
GenaroMazur <genaromazur03@gmail.com>
2023-02-08 16:57:39 -0300

    [FIRST]

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,11 @@ server.start().then((err)=>{
         res.send(devolver["hub.challenge"])
 
     })
+    server.app.post("/webhooks",(req,res)=>{
+        console.log(req.body);
+        
+        res.status(200).end()
+    })
     server.app.use(bodyParser.json())
 })
 .catch(()=>{
