477b409047d8d6485f786bf3af6cd194e3f81d00
GenaroMazur <genaromazur03@gmail.com>
2023-02-09 10:36:10 -0300

    [FEAT]

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ const server = server_1.SERVER.instance;
 const morgan_1 = __importDefault(require("morgan"));
 require("dotenv").config();
 const body_parser_1 = __importDefault(require("body-parser"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
 server.init();
 server.start().then((err) => {
     if (err)
@@ -18,6 +19,26 @@ server.start().then((err) => {
         console.log(req.query);
         const devolver = req.query;
         res.send(devolver["hub.challenge"]);
+    });
+    server.app.post("/webhooks", (req, res) => {
+        console.log(req.body);
+        let body = {
+            "messaging_product": "whatsapp",
+            "to": "3764560397",
+            "type": "template",
+            "text": {
+                "body": "Hola genaro"
+            }
+        };
+        body = JSON.stringify(body);
+        (0, node_fetch_1.default)("https://graph.facebook.com/v15.0/109330648741829/messages", {
+            method: "POST",
+            headers: {
+                authorization: "Bearer EAAIFmZAUGCsYBAFRDse3kHsaDtTpSznzjdA11ZAOYZAdLXnu6gIBdfG032PHIfF5J4EGp1KpWb4ml13IuqgdPojY2ZCkQFS3UvpwHPK1JXWS5oe5KdiPc8WGFPLAqHsJJAL7wPNfrCqviqzZAdxNU7VQ6yinw7B3PSih2DxnJ2xXveETZCPgq1vJUUO6C5l4gPTsi16AL9ZAQZDZD",
+            },
+            body
+        });
+        res.status(200).end();
     });
     server.app.use(body_parser_1.default.json());
 })
