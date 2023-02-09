309d959083d26eb6aab18ebbe53502daf8587360
GenaroMazur <genaromazur03@gmail.com>
2023-02-09 10:34:44 -0300

    [FEAT]

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,45 @@
 import { SERVER } from "./server";
 const server = SERVER.instance
 import morgan from "morgan"
-import { endpointResponse } from "./helpers/succes";
 require("dotenv").config()
 import bodyParser from "body-parser"
-
+import fetch from "node-fetch"
 server.init()
-server.start().then((err)=>{
-    if(err)throw new Error("error al iniciar servidor") && console.log(err)
-    
+server.start().then((err) => {
+    if (err) throw new Error("error al iniciar servidor") && console.log(err)
+
     server.app.use(morgan("dev"))
     server.app.use(bodyParser.json())
-    server.app.get("/webhooks",(req, res)=>{
+    server.app.get("/webhooks", (req, res) => {
         console.log(req.query);
-        const devolver:any = req.query
+        const devolver: any = req.query
 
         res.send(devolver["hub.challenge"])
 
     })
-    server.app.post("/webhooks",(req,res)=>{
+    server.app.post("/webhooks", (req, res) => {
         console.log(req.body);
-        
+        let body:any ={
+            "messaging_product": "whatsapp",
+            "to": "3764560397",
+            "type": "template",
+            "text":{
+                "body":"Hola genaro"
+            }
+        }
+        body = JSON.stringify(body)
+        fetch("https://graph.facebook.com/v15.0/109330648741829/messages",{
+            method:"POST",
+            headers:{
+                authorization:"Bearer EAAIFmZAUGCsYBAFRDse3kHsaDtTpSznzjdA11ZAOYZAdLXnu6gIBdfG032PHIfF5J4EGp1KpWb4ml13IuqgdPojY2ZCkQFS3UvpwHPK1JXWS5oe5KdiPc8WGFPLAqHsJJAL7wPNfrCqviqzZAdxNU7VQ6yinw7B3PSih2DxnJ2xXveETZCPgq1vJUUO6C5l4gPTsi16AL9ZAQZDZD",
+                
+            },
+            body
+        })
         res.status(200).end()
     })
     server.app.use(bodyParser.json())
 })
-.catch(()=>{
-    console.log("____________________________________________________");
-})
+    .catch(() => {
+        console.log("____________________________________________________");
+    })
