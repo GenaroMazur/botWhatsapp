00301ab4bd1a93aa9512fd95ae207994072344ca
GenaroMazur <genaromazur03@gmail.com>
2023-02-10 08:19:41 -0300

    [FEAT]

--- src/index.ts
+++ src/index.ts
@@ -20,32 +20,22 @@ server.start().then((err) => {
     server.app.post("/webhooks", (req, res) => {
         console.log(JSON.stringify(req.body.entry[0].changes[0]));
         console.log("_----------------------------------------------------------");
-        const header = "¡Bienvenido a Servicios Urbanos S.A!"
+        const header = "¡Bienvenido a Servicios Urbanos S.A!\n\n"
         const texto = "Para obtener un turno para tramitar el BEEG debe enviar los siguientes datos:\n1) Nombre y apellido.\n2) DNI\n3) Lugar de preferencia para realizar el trámite: Posadas Plaza Shopping o Terminal UNaM.\n4) Horario aprox. disponible\n\n*Posadas Plaza Shopping*: De Lunes a Sábados de 9:00hs a 21:00hs.\n\n*Terminal UNaM*: De Lunes a Viernes de 08:30hs a 13:00hs / 14:00hs a 17:30hs. Sábados de 08:00hs a 12:00hs.\n"
         const footer = "Por favor para otros tipos de consultas comunicarse al 0810-444-7823."
 
         let body: any = {
             "messaging_product": "whatsapp",
             "to": "543764560397",
-            "components": [
-                {
-                    "type": "header",
-                    "parameters": [
-                        {
-                            "type": "text",
-                            "text":`${header+footer}`
-                        }
-                    ]
-                },{
+            "components":{
                     "type":"body",
                     "parameters":[
                         {
                             "type":"text",
-                            "text":`${texto}`
+                            "text":`${header+texto+footer}`
                         }
                     ]
                 }
-            ]
         }
 
         body = JSON.stringify(body)
