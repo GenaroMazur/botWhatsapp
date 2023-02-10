d312e77e59fb5eb524695aae7bec6c3663093d9a
GenaroMazur <genaromazur03@gmail.com>
2023-02-10 09:14:36 -0300

    [FEAT]

--- dist/index.js
+++ dist/index.js
@@ -23,41 +23,63 @@ server.start().then((err) => {
     server.app.post("/webhooks", (req, res) => {
         console.log(JSON.stringify(req.body.entry[0].changes[0]));
         console.log("_----------------------------------------------------------");
-        const header = "¡Bienvenido a Servicios Urbanos S.A!";
-        const texto = "Para obtener un turno para tramitar el BEEG debe enviar los siguientes datos:\n1) Nombre y apellido.\n2) DNI\n3) Lugar de preferencia para realizar el trámite: Posadas Plaza Shopping o Terminal UNaM.\n4) Horario aprox. disponible\n\n*Posadas Plaza Shopping*: De Lunes a Sábados de 9:00hs a 21:00hs.\n\n*Terminal UNaM*: De Lunes a Viernes de 08:30hs a 13:00hs / 14:00hs a 17:30hs. Sábados de 08:00hs a 12:00hs.";
+        const header = "¡Bienvenido a Servicios Urbanos S.A!\n\n";
+        const texto = "Para obtener un turno para tramitar el BEEG debe enviar los siguientes datos:\n1) Nombre y apellido.\n2) DNI\n3) Lugar de preferencia para realizar el trámite: Posadas Plaza Shopping o Terminal UNaM.\n4) Horario aprox. disponible\n\n*Posadas Plaza Shopping*: De Lunes a Sábados de 9:00hs a 21:00hs.\n\n*Terminal UNaM*: De Lunes a Viernes de 08:30hs a 13:00hs / 14:00hs a 17:30hs. Sábados de 08:00hs a 12:00hs.\n";
         const footer = "Por favor para otros tipos de consultas comunicarse al 0810-444-7823.";
         let body = {
             "messaging_product": "whatsapp",
             "to": "543764560397",
-            "components": [
-                {
-                    "type": "header",
-                    "parameters": [
+            "type": "text",
+            "text": {
+                "body": `${header + texto + footer}`
+            }
+        };
+        let body3 = {
+            "messaging_product": "whatsapp",
+            "to": "543764560397",
+            "type": "interactive",
+            "interactive": {
+                "type": "list",
+                "body": { "text": "hello world" },
+                "action": {
+                    "button": "cta-button-content",
+                    "sections": [
                         {
-                            "type": "text",
-                            "text": `${header}`
-                        }
-                    ]
-                }, {
-                    "type": "body",
-                    "parameters": [
+                            "title": "your-section-title-content",
+                            "rows": [
+                                {
+                                    "id": "unique-row-identifierb",
+                                    "title": "row-title-content2",
+                                    "description": "row-description-content",
+                                }
+                            ]
+                        },
                         {
-                            "type": "text",
-                            "text": `${texto}`
-                        }
-                    ]
-                }, {
-                    "type": "footer",
-                    "parameters": [
-                        {
-                            "type": "text",
-                            "text": `${footer}`
+                            "title": "your-section-title-content",
+                            "rows": [
+                                {
+                                    "id": "unique-row-identifiera",
+                                    "title": "row-title-content3",
+                                    "description": "row-description-content",
+                                }
+                            ]
                         }
                     ]
                 }
-            ]
+            }
+        };
+        let body2 = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": "543764560397",
+            "type": "text",
+            "text": {
+                "header": { "text": "header" },
+                "body": { "text": `hola` }
+            }
         };
         body = JSON.stringify(body);
+        body2 = JSON.stringify(body2);
         const token = process.env.TOKEN || "";
         if (req.body.entry[0].changes[0].value.messages !== undefined) {
             console.log(req.body.entry[0].changes[0].value.messages);
@@ -67,8 +89,16 @@ server.start().then((err) => {
                     authorization: token,
                     "Content-Type": "application/json"
                 },
-                body
-            });
+                body: body3
+            }).then(response => console.log(response));
+            (0, cross_fetch_1.default)("https://graph.facebook.com/v15.0/109330648741829/messages", {
+                method: "POST",
+                headers: {
+                    authorization: token,
+                    "Content-Type": "application/json"
+                },
+                body: body2
+            }).then(response => console.log(response));
         }
         res.status(200).end();
     });
