f35811def5892ec8c1bdfb2398af2c3eaeb94dc3
GenaroMazur <genaromazur03@gmail.com>
2023-02-10 08:13:38 -0300

    [FEAT]

--- src/index.ts
+++ src/index.ts
@@ -20,29 +20,57 @@ server.start().then((err) => {
     server.app.post("/webhooks", (req, res) => {
         console.log(JSON.stringify(req.body.entry[0].changes[0]));
         console.log("_----------------------------------------------------------");
+        const header = "¡Bienvenido a Servicios Urbanos S.A!"
         const texto = "Para obtener un turno para tramitar el BEEG debe enviar los siguientes datos:\n1) Nombre y apellido.\n2) DNI\n3) Lugar de preferencia para realizar el trámite: Posadas Plaza Shopping o Terminal UNaM.\n4) Horario aprox. disponible\n\n*Posadas Plaza Shopping*: De Lunes a Sábados de 9:00hs a 21:00hs.\n\n*Terminal UNaM*: De Lunes a Viernes de 08:30hs a 13:00hs / 14:00hs a 17:30hs. Sábados de 08:00hs a 12:00hs."
+        const footer = "Por favor para otros tipos de consultas comunicarse al 0810-444-7823."
 
-        let body:any ={
+        let body: any = {
             "messaging_product": "whatsapp",
             "to": "543764560397",
-            "text":{"body":`${texto}`}
+            "components": [
+                {
+                    "type": "header",
+                    "parameters": [
+                        {
+                            "type": "text",
+                            "text":`${header}`
+                        }
+                    ]
+                },{
+                    "type":"body",
+                    "parameters":[
+                        {
+                            "type":"text",
+                            "text":`${texto}`
+                        }
+                    ]
+                },{
+                    "type":"footer",
+                    "parameters":[
+                        {
+                            "type":"text",
+                            "text":`${footer}`
+                    }
+                    ]
+                }
+            ]
         }
-        
+
         body = JSON.stringify(body)
         const token = process.env.TOKEN || ""
-        if(req.body.entry[0].changes[0].value.messages !== undefined){
+        if (req.body.entry[0].changes[0].value.messages !== undefined) {
             console.log(req.body.entry[0].changes[0].value.messages);
-            
-            fetch("https://graph.facebook.com/v15.0/109330648741829/messages",{
-                method:"POST",
-                headers:{
-                    authorization:token,
+
+            fetch("https://graph.facebook.com/v15.0/109330648741829/messages", {
+                method: "POST",
+                headers: {
+                    authorization: token,
                     "Content-Type": "application/json"
                 },
                 body
             })
         }
-        
+
         res.status(200).end()
     })
     server.app.use(bodyParser.json())
