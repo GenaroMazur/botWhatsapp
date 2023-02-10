0c2c1b7fc5ccea381b1a4cabf10792d2cf14dfdd
GenaroMazur <genaromazur03@gmail.com>
2023-02-10 08:17:16 -0300

    [FEAT]

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ server.start().then((err) => {
         console.log(JSON.stringify(req.body.entry[0].changes[0]));
         console.log("_----------------------------------------------------------");
         const header = "¡Bienvenido a Servicios Urbanos S.A!"
-        const texto = "Para obtener un turno para tramitar el BEEG debe enviar los siguientes datos:\n1) Nombre y apellido.\n2) DNI\n3) Lugar de preferencia para realizar el trámite: Posadas Plaza Shopping o Terminal UNaM.\n4) Horario aprox. disponible\n\n*Posadas Plaza Shopping*: De Lunes a Sábados de 9:00hs a 21:00hs.\n\n*Terminal UNaM*: De Lunes a Viernes de 08:30hs a 13:00hs / 14:00hs a 17:30hs. Sábados de 08:00hs a 12:00hs."
+        const texto = "Para obtener un turno para tramitar el BEEG debe enviar los siguientes datos:\n1) Nombre y apellido.\n2) DNI\n3) Lugar de preferencia para realizar el trámite: Posadas Plaza Shopping o Terminal UNaM.\n4) Horario aprox. disponible\n\n*Posadas Plaza Shopping*: De Lunes a Sábados de 9:00hs a 21:00hs.\n\n*Terminal UNaM*: De Lunes a Viernes de 08:30hs a 13:00hs / 14:00hs a 17:30hs. Sábados de 08:00hs a 12:00hs.\n"
         const footer = "Por favor para otros tipos de consultas comunicarse al 0810-444-7823."
 
         let body: any = {
@@ -33,7 +33,7 @@ server.start().then((err) => {
                     "parameters": [
                         {
                             "type": "text",
-                            "text":`${header}`
+                            "text":`${header+footer}`
                         }
                     ]
                 },{
@@ -43,14 +43,6 @@ server.start().then((err) => {
                             "type":"text",
                             "text":`${texto}`
                         }
-                    ]
-                },{
-                    "type":"footer",
-                    "parameters":[
-                        {
-                            "type":"text",
-                            "text":`${footer}`
-                    }
                     ]
                 }
             ]
