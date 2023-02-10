409450568eb2376c3512eedc7c4d9d9d780cd826
GenaroMazur <genaromazur03@gmail.com>
2023-02-10 13:47:46 -0300

    [FEAT]

--- dist/routes/index.routes.js
+++ dist/routes/index.routes.js
@@ -8,9 +8,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.indexRouter = void 0;
+const cross_fetch_1 = __importDefault(require("cross-fetch"));
 const indexRouter = (message) => __awaiter(void 0, void 0, void 0, function* () {
+    const messageId = message.entry[0].changes[0].value.messages.messages[0].id;
+    let responseRead = JSON.stringify({ status: "read" });
+    (0, cross_fetch_1.default)("https://graph.facebook.com/v15.0/109330648741829/messages/" + messageId, {
+        method: "PUT",
+        body: responseRead
+    });
     if (message.entry[0].changes[0].value.errors !== undefined) {
         console.log("¡ SUCEDIO UN PROBLEMA !");
         console.error(message.entry[0].changes[0].value.errors);
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,14 +1,21 @@
 import { comunMessage } from "../interfaces/interfaces";
 import { whastappObjectResponse } from "../interfaces/whatsappResponseInterface";
 import { sendToNum } from "../service/sendMessajeToNum";
+import fetch from "cross-fetch"
 
-export const indexRouter =async (message:whastappObjectResponse):Promise<void> => {
-    if(message.entry[0].changes[0].value.errors !== undefined){
+export const indexRouter = async (message: whastappObjectResponse): Promise<void> => {
+    const messageId = message.entry[0].changes[0].value.messages.messages[0].id
+    let responseRead = JSON.stringify({status: "read"})
+    fetch("https://graph.facebook.com/v15.0/109330648741829/messages/" + messageId, {
+        method: "PUT",
+        body:responseRead
+    })
+    if (message.entry[0].changes[0].value.errors !== undefined) {
         console.log("¡ SUCEDIO UN PROBLEMA !");
         console.error(message.entry[0].changes[0].value.errors)
         return
     }
-    
+
     console.log(message.entry[0].changes[0].value.messages);
     console.log(message.entry[0].changes[0].value);
     console.log(message.entry[0].changes[0].value.errors);
