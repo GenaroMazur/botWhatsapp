24a4cadd1325eddcab92ce01a44663f0e6e116d9
GenaroMazur <genaromazur03@gmail.com>
2023-02-10 14:09:28 -0300

    [FEAT]

--- dist/routes/index.routes.js
+++ dist/routes/index.routes.js
@@ -14,13 +14,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.indexRouter = void 0;
 const cross_fetch_1 = __importDefault(require("cross-fetch"));
+require("dotenv").config();
 const indexRouter = (message) => __awaiter(void 0, void 0, void 0, function* () {
+    const token = process.env.TOKEN || "";
     const messageId = message.entry[0].changes[0].value.messages[0].id;
-    let responseRead = JSON.stringify({ status: "read" });
-    (0, cross_fetch_1.default)("https://graph.facebook.com/v15.0/109330648741829/messages/" + messageId, {
+    let responseRead = {
+        "headers": {
+            "authorization": token,
+            "Content-Type": "application/json"
+        },
         method: "PUT",
-        body: responseRead
-    }).then(r => console.log(r));
+        body: JSON.stringify({ status: "read" })
+    };
+    (0, cross_fetch_1.default)("https://graph.facebook.com/v15.0/109330648741829/messages/" + messageId, responseRead).then(r => console.log(r));
     if (message.entry[0].changes[0].value.errors !== undefined) {
         console.log("¡ SUCEDIO UN PROBLEMA !");
         console.error(message.entry[0].changes[0].value.errors);
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -3,13 +3,22 @@ import { whastappObjectResponse } from "../interfaces/whatsappResponseInterface"
 import { sendToNum } from "../service/sendMessajeToNum";
 import fetch from "cross-fetch"
 
+require("dotenv").config()
 export const indexRouter = async (message: whastappObjectResponse): Promise<void> => {
+    const token = process.env.TOKEN || ""
     const messageId = message.entry[0].changes[0].value.messages[0].id
-    let responseRead = JSON.stringify({status: "read"})
-    fetch("https://graph.facebook.com/v15.0/109330648741829/messages/" + messageId, {
+    
+    let responseRead = {
+        "headers":{
+            "authorization":token,
+            "Content-Type":"application/json"
+        },
         method: "PUT",
-        body:responseRead
-    }).then(r=>console.log(r))
+        body:JSON.stringify({status: "read"})
+    }
+
+
+    fetch("https://graph.facebook.com/v15.0/109330648741829/messages/" + messageId, responseRead).then(r=>console.log(r))
     if (message.entry[0].changes[0].value.errors !== undefined) {
         console.log("¡ SUCEDIO UN PROBLEMA !");
         console.error(message.entry[0].changes[0].value.errors)
