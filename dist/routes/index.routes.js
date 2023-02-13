c727c5ba826e5e2d40dd8d2d33d974a0eeaa4ed0
GenaroMazur <genaromazur03@gmail.com>
2023-02-13 10:57:33 -0300

    [FEAT]

--- dist/routes/index.routes.js
+++ dist/routes/index.routes.js
@@ -16,12 +16,12 @@ exports.indexRouter = void 0;
 const cross_fetch_1 = __importDefault(require("cross-fetch"));
 require("dotenv").config();
 const indexRouter = (message) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m;
+    var _a, _b, _c, _d, _e, _f;
     const token = process.env.TOKEN || "";
-    console.log(message);
+    console.log(message.entry[0].changes[0].value);
     const messageId = (_c = (_b = (_a = message.entry[0]) === null || _a === void 0 ? void 0 : _a.changes[0]) === null || _b === void 0 ? void 0 : _b.value.messages[0]) === null || _c === void 0 ? void 0 : _c.id;
-    console.log("ESTADO : ", (_g = (_f = (_e = (_d = message.entry[0]) === null || _d === void 0 ? void 0 : _d.changes[0]) === null || _e === void 0 ? void 0 : _e.value) === null || _f === void 0 ? void 0 : _f.statuses[0]) === null || _g === void 0 ? void 0 : _g.status);
-    console.log("TIPO : ", (_j = (_h = message.entry[0]) === null || _h === void 0 ? void 0 : _h.changes[0]) === null || _j === void 0 ? void 0 : _j.field);
+    // console.log("ESTADO : "+message.entry[0].changes[0].value?.statuses[0].status)
+    // console.log("TIPO : "+message.entry[0].changes[0].field)
     if (messageId !== undefined) {
         let responseRead = {
             "headers": {
@@ -33,7 +33,7 @@ const indexRouter = (message) => __awaiter(void 0, void 0, void 0, function* () 
         };
         (0, cross_fetch_1.default)("https://graph.facebook.com/v16.0/109330648741829/messages", responseRead);
     }
-    if (((_m = (_l = (_k = message.entry[0]) === null || _k === void 0 ? void 0 : _k.changes[0]) === null || _l === void 0 ? void 0 : _l.value) === null || _m === void 0 ? void 0 : _m.errors) !== undefined) {
+    if (((_f = (_e = (_d = message.entry[0]) === null || _d === void 0 ? void 0 : _d.changes[0]) === null || _e === void 0 ? void 0 : _e.value) === null || _f === void 0 ? void 0 : _f.errors) !== undefined) {
         console.log("¡ SUCEDIO UN PROBLEMA !");
         console.error(message.entry[0].changes[0].value.errors);
         return;
