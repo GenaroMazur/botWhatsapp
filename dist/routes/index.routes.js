8a220fd4beacc719d05e5c609e605228dde91f88
GenaroMazur <genaromazur03@gmail.com>
2023-02-13 10:45:25 -0300

    [FEAT]

--- dist/routes/index.routes.js
+++ dist/routes/index.routes.js
@@ -16,11 +16,11 @@ exports.indexRouter = void 0;
 const cross_fetch_1 = __importDefault(require("cross-fetch"));
 require("dotenv").config();
 const indexRouter = (message) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b, _c, _d, _e;
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
     const token = process.env.TOKEN || "";
-    const messageId = (_b = (_a = message.entry[0].changes[0]) === null || _a === void 0 ? void 0 : _a.value.messages[0]) === null || _b === void 0 ? void 0 : _b.id;
-    console.log("ESTADO : ", (_d = (_c = message.entry[0].changes[0]) === null || _c === void 0 ? void 0 : _c.value.statuses[0]) === null || _d === void 0 ? void 0 : _d.status);
-    console.log("ESTADO : ", (_e = message.entry[0].changes[0]) === null || _e === void 0 ? void 0 : _e.field);
+    const messageId = (_c = (_b = (_a = message.entry[0]) === null || _a === void 0 ? void 0 : _a.changes[0]) === null || _b === void 0 ? void 0 : _b.value.messages[0]) === null || _c === void 0 ? void 0 : _c.id;
+    console.log("ESTADO : ", (_f = (_e = (_d = message.entry[0]) === null || _d === void 0 ? void 0 : _d.changes[0]) === null || _e === void 0 ? void 0 : _e.value.statuses[0]) === null || _f === void 0 ? void 0 : _f.status);
+    console.log("ESTADO : ", (_h = (_g = message.entry[0]) === null || _g === void 0 ? void 0 : _g.changes[0]) === null || _h === void 0 ? void 0 : _h.field);
     if (messageId !== undefined) {
         let responseRead = {
             "headers": {
@@ -32,7 +32,7 @@ const indexRouter = (message) => __awaiter(void 0, void 0, void 0, function* () 
         };
         (0, cross_fetch_1.default)("https://graph.facebook.com/v16.0/109330648741829/messages", responseRead);
     }
-    if (message.entry[0].changes[0].value.errors !== undefined) {
+    if (((_k = (_j = message.entry[0].changes[0]) === null || _j === void 0 ? void 0 : _j.value) === null || _k === void 0 ? void 0 : _k.errors) !== undefined) {
         console.log("¡ SUCEDIO UN PROBLEMA !");
         console.error(message.entry[0].changes[0].value.errors);
         return;
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -4,9 +4,9 @@ import fetch from "cross-fetch"
 require("dotenv").config()
 export const indexRouter = async (message: whastappObjectResponse): Promise<void> => {
     const token = process.env.TOKEN || ""
-    const messageId = message.entry[0].changes[0]?.value.messages[0]?.id
-    console.log("ESTADO : ",message.entry[0].changes[0]?.value.statuses[0]?.status)
-    console.log("ESTADO : ",message.entry[0].changes[0]?.field)
+    const messageId = message.entry[0]?.changes[0]?.value.messages[0]?.id
+    console.log("ESTADO : ",message.entry[0]?.changes[0]?.value.statuses[0]?.status)
+    console.log("ESTADO : ",message.entry[0]?.changes[0]?.field)
     if (messageId !== undefined) {
         let responseRead = {
             "headers": {
@@ -19,7 +19,7 @@ export const indexRouter = async (message: whastappObjectResponse): Promise<void
         fetch("https://graph.facebook.com/v16.0/109330648741829/messages", responseRead)
     }
 
-    if (message.entry[0].changes[0].value.errors !== undefined) {
+    if (message.entry[0].changes[0]?.value?.errors !== undefined) {
         console.log("¡ SUCEDIO UN PROBLEMA !");
         console.error(message.entry[0].changes[0].value.errors)
         return
