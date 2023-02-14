159171273a068eb2b62364dc3aeb7cb29c4ad222
GenaroMazur <genaromazur03@gmail.com>
2023-02-14 13:59:15 -0300

    [FEAT]

--- src/shared/modelsMessages.ts
+++ src/shared/modelsMessages.ts
@@ -26,7 +26,7 @@ export const dniModel = (num:number)=>{
 
 let turnsDays:Array<string> = []
 for (let x=1;x<11;x++){
-    turnsDays.push(dateZoneString(dateNowTimestamp() + 60 * 60 * 24 * x, 'zu-ZA', 'America/Argentina/Cordoba').split(" ")[0].slice(4))
+    turnsDays.push(dateZoneString(dateNowTimestamp() + 60 * 60 * 24 * x, 'zu-ZA', 'America/Argentina/Cordoba').split(" ")[0].slice(5))
 }
 export const datesModels = async (num:number)=>{
     let query: any = {
@@ -57,7 +57,7 @@ export const datesModels = async (num:number)=>{
     }
     
     turnsDays.forEach((day,index)=>{
-        listDate.interactive.action.sections[0].rows.push({"id":index.toString(), "title":day, "description":"disponible"})
+        listDate.interactive.action.sections[0].rows.push({"id":index.toString(), "title":day, "description":`Mes ${day.split("-")[0]}, dia ${day.split("-")[1]}`})
     })
     console.log(listDate.interactive.action.sections[0].rows);
     
