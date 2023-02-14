0fd131a6be19806688723cee8a7578a0c0b92a91
GenaroMazur <genaromazur03@gmail.com>
2023-02-14 14:04:40 -0300

    [FEAT]

--- src/shared/proccesMessage.ts
+++ src/shared/proccesMessage.ts
@@ -5,65 +5,68 @@ import { whastappObjectResponse } from "../interfaces/whatsappResponseInterface"
 import { comunMessage, turnInterface } from "../interfaces/interfaces"
 const nums = "0123456789"
 
-export const processMessage = async(text:string, num:number, conversation:turnInterface, key:string)=>{
-    const userMessage:string = text.toLowerCase()
+export const processMessage = async (text: string, num: number, conversation: turnInterface, key: string) => {
+    const userMessage: string = text.toLowerCase()
     console.log(conversation);
-    if(conversation.fullName===""){
+    if (conversation.fullName === "") {
 
-        if(userMessage.length>4){
-            conversation.fullName=text
+        if (userMessage.length > 4) {
+            conversation.fullName = text
             await nodePersist.updateItem(key, conversation)
             sendToUser(JSON.stringify(dniModel(num)))
         } else {
-            const errorMessage:comunMessage={
-                "messaging_product":"whatsapp",
-                "text":{"body":"nombre invalido, debe ser mayor a 4 caracteres"},
-                "type":"text",
-                "to":num.toString()
+            const errorMessage: comunMessage = {
+                "messaging_product": "whatsapp",
+                "text": { "body": "nombre invalido, debe ser mayor a 4 caracteres" },
+                "type": "text",
+                "to": num.toString()
             }
             sendToUser(JSON.stringify(errorMessage))
         }
 
-    } else if(conversation.fullName===null){
+    } else if (conversation.fullName === null) {
 
-        conversation.fullName=""
+        conversation.fullName = ""
         await nodePersist.updateItem(key, conversation)
         sendToUser(JSON.stringify(welcomeModel(num)))
 
-    } else if(conversation.fullName!=="" && conversation.fullName!==null){
+    } else if (conversation.fullName !== "" && conversation.fullName !== null) {
 
-        if((userMessage.length==7 || userMessage.length==8) && userMessage.split("").every(value=>nums.includes(value))){
-            conversation.document=userMessage
+        if ((userMessage.length == 7 || userMessage.length == 8) && userMessage.split("").every(value => nums.includes(value))) {
+            conversation.document = userMessage
             await nodePersist.updateItem(key, conversation)
             sendToUser(JSON.stringify(await datesModels(num)))
         } else {
-            const errorMessage:comunMessage={
-                "messaging_product":"whatsapp",
-                "text":{"body":"Numero de DNI invalido"},
-                "type":"text",
-                "to":num.toString()
+            const errorMessage: comunMessage = {
+                "messaging_product": "whatsapp",
+                "text": { "body": "Numero de DNI invalido" },
+                "type": "text",
+                "to": num.toString()
             }
             sendToUser(JSON.stringify(errorMessage))
         }
 
-    }
-    
-    else {
-        const errorMessage:comunMessage={
-            "messaging_product":"whatsapp",
-            "text":{"body":"No entiendo su peticion"},
-            "type":"text",
-            "to":num.toString()
+    } else if (conversation.document !== "") {
+        conversation.date = userMessage
+        console.log(userMessage);
+        await nodePersist.updateItem(key, conversation)
+        sendToUser(JSON.stringify(await datesModels(num)))
+    } else {
+        const errorMessage: comunMessage = {
+            "messaging_product": "whatsapp",
+            "text": { "body": "No entiendo su peticion" },
+            "type": "text",
+            "to": num.toString()
         }
         sendToUser(JSON.stringify(errorMessage))
     }
 }
 
-export const persistConversation = async (message:whastappObjectResponse)=>{
+export const persistConversation = async (message: whastappObjectResponse) => {
     const cellphoneNum = message.entry[0].changes[0].value.messages[0].from
-    const conversation:turnInterface = await nodePersist.getItem(cellphoneNum)
-    const turn:turnInterface = {"fullName":null, "document":"", "date":"", "hour":"", "place":""}
-    if(conversation === undefined){
+    const conversation: turnInterface = await nodePersist.getItem(cellphoneNum)
+    const turn: turnInterface = { "fullName": null, "document": "", "date": "", "hour": "", "place": "" }
+    if (conversation === undefined) {
         await nodePersist.setItem(cellphoneNum, turn)
     }
     return conversation || turn
