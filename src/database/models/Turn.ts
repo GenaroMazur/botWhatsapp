fc8338d11e53172552a4dd9d18dd1c2ed4f44223
GenaroMazur <genaromazur03@gmail.com>
2023-02-16 15:20:24 -0300

    [FEAT]

--- src/database/models/Turn.ts
+++ src/database/models/Turn.ts
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose"
 
 const turnsSchema = new Schema({
-    _id:{type:Schema.Types.ObjectId},
+    _id:{type:Schema.Types.ObjectId, required:true},
     hour:{type:String, required:true},
     reserved:{type:Boolean, default:false },
     cellphoneNumber:{type:String, default:""},
