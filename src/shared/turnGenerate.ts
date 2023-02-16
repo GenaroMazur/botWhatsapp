b01b7b1441ecc969c944f072df6280fb57d23fb4
GenaroMazur <genaromazur03@gmail.com>
2023-02-16 10:11:32 -0300

    [FEAT]

--- src/shared/turnGenerate.ts
+++ src/shared/turnGenerate.ts
@@ -13,6 +13,8 @@ export const generateTurns = async (creation: creationForm) => {
     const openHourEvening = parseInt(creation.evening.start.substring(0, 2))
     const closeHourEvening = parseInt(creation.evening.end.substring(0, 2))
     const openMinutesEvening = creation.evening.start.substring(3, 5)
+    console.log("inicializar horas");
+    
     let turn: turnInterface = {
         "cellphoneNumber": "",
         "document": "",
@@ -25,17 +27,20 @@ export const generateTurns = async (creation: creationForm) => {
     const eveningBoxxes = creation.evening.boxxes
     let date = new Date(`${new Date().getFullYear()}-${creation.dateStart}`)
     await NodePersist.setItem("openTurns", [])
-
+    
+    console.log("crear archivo persistente");
     for (let x = 1; x <= daysDiff; x++) {
-
+        console.log("bucle dia num:",x);
+        
         let openTurns: Array<turnInterface> = []
         if (creation.daysOfWeek.includes(daysOfWeek[date.getDay()])) {
-
+            
             turn.turn = "mañana"
             turn.hour = `${openHourMorning}:${openMinutesMorning}hs`
             openTurns.push(turn)
-
+            
             for (let y = openHourMorning; y < closeHourMorning; y++) {
+                console.log("bucle hora mañana num:",x);
                 let minutes = y === openHourMorning ? parseInt(turn.hour.substring(3, 5)) : 0
 
                 while (minutes < 60 && minutes + 6 - morningBoxxes < 60) {
@@ -49,8 +54,9 @@ export const generateTurns = async (creation: creationForm) => {
             turn.turn = "tarde"
             turn.hour = `${openHourEvening}:${openMinutesEvening}hs`
             openTurns.push(turn)
-
+            
             for (let y = openHourEvening; y < closeHourEvening; y++) {
+                console.log("bucle hora tarde num:",x);
                 let minutes = y === openHourEvening ? parseInt(turn.hour.substring(3, 5)) : 0
 
                 while (minutes < 60 || minutes + 6 - eveningBoxxes < 60) {
