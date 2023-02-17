31469bdf9e3f8755db4ba8e0344d6800a8ca3935
GenaroMazur <genaromazur03@gmail.com>
2023-02-17 12:36:04 -0300

    [FEAT]

--- src/shared/modelsMessages.ts
+++ src/shared/modelsMessages.ts
@@ -128,13 +128,9 @@ export const momentModel = (num: number) => {
 
 export const turnReady = async (num: number, conversation: conversationInterface, hourRange:Array<string>) => {
 
-    const turns:any = (await Turn.find({"date":conversation.date, "place":conversation.place}).select({turns:{$elemMatch:{hour:new RegExp("8","i")}}}))
-    console.log(await Turn.find({"date":conversation.date, "place":conversation.place},{turns:{$elemMatch:{hour:new RegExp("8","i")}}}))
+    const turns:any = (await Turn.find({"date":conversation.date, "place":conversation.place}).select({turns:{$elemMatch:{hour:new RegExp("8","i")}}}))[0]?.turns
     console.log(turns);
-    const selectedTurn = turns?.find((turn:any)=>{
-        return (!turn.reserved) && turn.hour>hourRange[0]
-    })?.hour
-    const message = `*Su turno fue asignado el día ${conversation.date} a las ${selectedTurn}.en ${conversation.place} 
+    const message = `*Su turno fue asignado el día ${conversation.date} a las ${turns.hour}.en ${conversation.place} 
 
     ⚠️Recuerde que para realizar el trámite debe contar con la siguiente documentación obligatoria:
     1) Constancia de Inscripción BEEG 2022. (Firmado y Sellado por la institución escolar y policial). Obtenida del sitio oficial https://beg.misiones.gov.ar/#/home
